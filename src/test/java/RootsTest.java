3c902c45bb4f6086ddc99e5b161f3b52c8aef08b
Merrick <merrickm@live.com>
2026-06-05 15:48:05 -0400

    HW 5 Effort.

--- src/test/java/RootsTest.java
+++ src/test/java/RootsTest.java
@@ -5,20 +5,25 @@ import org.junit.jupiter.api.Test;
 
 public class RootsTest {
 
-    private static final double DELTA = 1e-9;
+    private static final double DELTA = 1e-15;
 
     @Test
-    void aNotZero_qPositive_hasTwoRoots() {
+    void qPositive_aNotZero() {
         Roots.calculate_roots(1, -3, 2);
 
+        System.out.println("a: 1, b: -3, c: 2");
+        System.out.println("Number of roots: " + Roots.num_roots() + ", First root: " + Roots.first_root() + ", Second root: " + Roots.second_root());
+        
         assertEquals(2.0, Roots.num_roots(), DELTA);
         assertEquals(2.0, Roots.first_root(), DELTA);
         assertEquals(1.0, Roots.second_root(), DELTA);
     }
 
     @Test
-    void aNotZero_qZero_hasOneRoot() {
+    void qZero_aNotZero() {
         Roots.calculate_roots(1, 2, 1);
+        System.out.println("a: 1, b: 2, c: 1");
+        System.out.println("Number of roots: " + Roots.num_roots() + ", First root: " + Roots.first_root() + ", Second root: " + Roots.second_root());
 
         assertEquals(1.0, Roots.num_roots(), DELTA);
         assertEquals(-1.0, Roots.first_root(), DELTA);
@@ -26,8 +31,10 @@ public class RootsTest {
     }
 
     @Test
-    void aNotZero_qNegative_hasNoRoots() {
+    void qNegative_aNotZero() {
         Roots.calculate_roots(1, 0, 1);
+        System.out.println("a: 1, b: 0, c: 1");
+        System.out.println("Number of roots: " + Roots.num_roots() + ", First root: " + Roots.first_root() + ", Second root: " + Roots.second_root());
 
         assertEquals(0.0, Roots.num_roots(), DELTA);
         assertEquals(-1.0, Roots.first_root(), DELTA);
@@ -35,8 +42,10 @@ public class RootsTest {
     }
 
     @Test
-    void aZero_qPositive_currentCodeReturnsNoRootsSentinelValues() {
+    void qPositive_aZero_currentCodeReturnsNoRootsSentinelValues() {
         Roots.calculate_roots(0, 2, 1);
+        System.out.println("a: 0, b: 2, c: 1");
+        System.out.println("Number of roots: " + Roots.num_roots() + ", First root: " + Roots.first_root() + ", Second root: " + Roots.second_root());
 
         assertEquals(0.0, Roots.num_roots(), DELTA);
         assertEquals(-1.0, Roots.first_root(), DELTA);
@@ -44,8 +53,10 @@ public class RootsTest {
     }
 
     @Test
-    void aZero_qZero_currentCodeProducesNaNRoots() {
+    void qZero_aZero_currentCodeProducesNaNRoots() {
         Roots.calculate_roots(0, 0, 5);
+        System.out.println("a: 0, b: 0, c: 5");
+        System.out.println("Number of roots: " + Roots.num_roots() + ", First root: " + Roots.first_root() + ", Second root: " + Roots.second_root());
 
         assertEquals(1.0, Roots.num_roots(), DELTA);
         assertTrue(Double.isNaN(Roots.first_root()));
